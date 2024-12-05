8bf93b40d5cbc965153f9fea01589fcb460c18a4
Brandon Snider <brandon@snider.so>
2024-12-05 10:58:41 +0000

    Eval Script

--- eval.sh
+++ eval.sh
@@ -1,23 +1,49 @@
-python /root/OpenCodeEval/src/main.py \
-    --model_name EleutherAI/pythia-160m \
+python src/main.py \
+    --model_name Qwen/Qwen2.5-Coder-0.5B \
     --trust_remote_code \
     --task HumanEval \
     --prompt_type Completion \
     --model_type Base \
-    --save_path /root/pruning-vol/evals/pythia-160m/humaneval
+    --save_path out/evals/qwen-0.5b/humaneval \
+    --temperature 0.1
 
-python /root/OpenCodeEval/src/main.py \
-    --model_name EleutherAI/pythia-160m \
-    --trust_remote_code \
-    --task LeetCode \
-    --prompt_type Completion \
-    --model_type Base \
-    --save_path /root/pruning-vol/evals/pythia-160m/leetcode
+# python src/main.py \
+#     --model_name EleutherAI/pythia-160m \
+#     --trust_remote_code \
+#     --task LeetCode \
+#     --prompt_type Completion \
+#     --model_type Base \
+#     --save_path out/evals/pythia-160m/leetcode
 
-python /root/OpenCodeEval/src/main.py \
-    --model_name EleutherAI/pythia-160m \
-    --trust_remote_code \
-    --task MBPP \
-    --prompt_type Instruction \          
-    --model_type Base \
-    --save_path /root/pruning-vol/evals/pythia-160m/mbpp
+# python src/main.py \
+#     --model_name EleutherAI/pythia-160m \
+#     --trust_remote_code \
+#     --task MBPP \
+#     --prompt_type Instruction \
+#     --model_type Base \
+#     --save_path out/evals/pythia-160m/mbpp
+
+# python src/main.py \
+#     --model_name ~/az-fs4/LLM-Shearing/out/prune_pythia/pythia_160m__70m_2048_opencoder-annealing/hf \
+#     --trust_remote_code \
+#     --task HumanEval \
+#     --prompt_type Completion \
+#     --model_type Base \
+#     --save_path out/evals/pythia-70m-pruned/humaneval \
+#     --temperature 0.1
+
+# python src/main.py \
+#     --model_name ~/az-fs4/LLM-Shearing/out/prune_pythia/pythia_160m__70m_2048_opencoder-annealing/hf \
+#     --trust_remote_code \
+#     --task LeetCode \
+#     --prompt_type Completion \
+#     --model_type Base \
+#     --save_path out/evals/pythia-70m-pruned/leetcode
+
+# python src/main.py \
+#     --model_name ~/az-fs4/LLM-Shearing/out/prune_pythia/pythia_160m__70m_2048_opencoder-annealing/hf \
+#     --trust_remote_code \
+#     --task MBPP \
+#     --prompt_type Instruction \
+#     --model_type Base \
+#     --save_path out/evals/pythia-70m-pruned/mbpp
